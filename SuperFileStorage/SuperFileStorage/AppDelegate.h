1201462cf8e32603a6d05b18723dc448e6bd1608
SuperCabbage <289646686@qq.com>
2018-04-18 14:02:07 +0800

    项目初始化

---
+++ SuperFileStorage/SuperFileStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperFileStorage
+//
+//  Created by SuperCabbage on 2018/4/18.
+//  Copyright © 2018年 SuperCabbage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
